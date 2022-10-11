ea098318210b6f0a7f7ff71fa535105ba47d5582
Akil Chowdhury <akiluddinchy@gmail.com>
2022-10-11 16:05:52 +0600

    made chart responsive

--- src/components/Barchart/Barchart.js
+++ src/components/Barchart/Barchart.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Barchart = ({data}) => {
 
     return (
-        <div className="flex justify-center">
-            <BarChart width={500} height={500} data={data}>
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Bar dataKey="total" fill=" #009900" />
-            </BarChart>
+        <div className="flex justify-center w-full h-full p-4 lg:w-1/2 lg:h-1/2">
+            <ResponsiveContainer width="100%" height="100%">
+                <BarChart width={500} height={500} data={data}>
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Bar dataKey="total" fill=" #009900" />
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
+        <div className="h-screen">
             <h1>blog</h1>
         </div>
     );
