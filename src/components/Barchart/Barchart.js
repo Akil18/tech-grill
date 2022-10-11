42a468fe3efa15321579f13b0397adb379e736f1
Akil Chowdhury <akiluddinchy@gmail.com>
2022-10-11 16:25:41 +0600

    added blog page

--- src/components/Barchart/Barchart.js
+++ src/components/Barchart/Barchart.js
@@ -4,7 +4,8 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recha
 const Barchart = ({data}) => {
 
     return (
-        <div className="flex justify-center w-full h-full p-4 lg:w-1/2 lg:h-1/2">
+        <div className="w-full h-full flex justify-center">
+            <div className="w-full h-full p-4 lg:w-1/2 lg:h-1/2">
             <ResponsiveContainer width="100%" height="100%">
                 <BarChart width={500} height={500} data={data}>
                     <XAxis dataKey="name" />
@@ -13,6 +14,7 @@ const Barchart = ({data}) => {
                     <Bar dataKey="total" fill=" #009900" />
                 </BarChart>
             </ResponsiveContainer>
+        </div>
         </div>
     );
 };
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div className="h-screen">
-            <h1>blog</h1>
+        <div className="h-screen m-4 lg:m-32 text-left">
+            <div>
+                <p className="text-xl my-8">What is the purpose of react router</p>
+                <p>React Router is standard library to enable routing within the react app. It allows navigation between views of different components, changing the browser URL and keeping the user interface in sync with the change in the URL.</p>
+            </div>
+            <div>
+                <p className="text-xl my-8">How does Context API work?</p>
+                <p>The Context API is used to create global variables that can be passed around quite easily avoiding the the alternative process of prop drilling. It is a easier and lighter way of passing data between components.</p>
+            </div>
+            <div>
+                <p className="text-xl my-8">Define useRef</p>
+                <p>useRef is ahook in react that enables to create a reference to the DOM element in the functional component. useRef returns a ref object that can be mutated.</p>
+            </div>
         </div>
     );
 };
